9b13244923270f143a8d9799ffebdcba4dcb9140
Raza <Raza@Raza-PC>
2016-04-18 01:25:17 +0530

    Tiles Framework Added

---
+++ Mart/WebContent/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="US-ASCII"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>${message}</h2>
+Welcome to Mart index
+</body>
+</html>
--- Mart/src/com/mart/controller/HelloController.java
+++ Mart/src/com/mart/controller/HelloController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/hello")
 public class HelloController{
  
-   @RequestMapping(method = RequestMethod.GET)
+  // @RequestMapping(method = RequestMethod.GET)
+	@RequestMapping("/hello")
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
       return "hello";
+   }
+   @RequestMapping("/index" )
+   public String index(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework! from index controller");
+      return "index";
    }
 
 }
